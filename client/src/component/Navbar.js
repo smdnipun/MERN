475c6cbb40d7e9d2d668c046ed0d1490392d53ac
Hiroshac <hiroshachandramal01@gmail.com>
2022-05-21 11:25:06 +0530

    ADD

--- client/src/component/Navbar.js
+++ client/src/component/Navbar.js
@@ -2,17 +2,35 @@ import React from "react";
 
 function Navbar() {
     return(
-        <ul class="nav justify-content-end">
-            <li class="nav-item">
-                <a class="nav-link active" href="/home">Home</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="/reg">Reg</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="/contact">Contact</a>
-            </li>
-      </ul>
+
+
+<ul class="nav flex-column">
+
+<li class="nav-item">
+
+  <a class="nav-link active" href="#">Active</a>
+
+</li>
+
+<li class="nav-item">
+
+  <a class="nav-link" href="#">Link</a>
+
+</li>
+
+<li class="nav-item">
+
+  <a class="nav-link" href="#">Link</a>
+
+</li>
+
+<li class="nav-item">
+
+  <a class="nav-link disabled" href="#">Disabled</a>
+
+</li>
+
+</ul>
     )
 }
 export default Navbar;
