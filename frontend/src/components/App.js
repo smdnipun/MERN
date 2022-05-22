8a5f3d30a15d7849037baef3d71df9f8c5a34ac7
pamudithaKekulandara <pkekulandera@gmail.com>
2022-05-22 09:49:24 +0530

    add

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -18,7 +18,7 @@ const App =()=>{
            <Route exact path={'/login'} element={<Login/>}/>
             </Routes>    
 
-            <NavBar/>
+           
             
            {/* <Fpage/> */}
          
