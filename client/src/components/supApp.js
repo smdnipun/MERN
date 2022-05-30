3c5bd33c7e6c8b40f535cc739bab4cdbc8c9ed40
smdnipun <smdnipun@gmail.com>
2022-05-31 01:02:30 +0530

    accept topics

--- client/src/components/supApp.js
+++ client/src/components/supApp.js
@@ -1,18 +1,18 @@
-import React from "react";
-import { Route,Routes } from "react-router-dom";
-import SuppervisorDash from "./supervisour/SuppervisorDash";
+import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import AcceptTopic from './supervisour/acceptTopic'
+import SuppervisorDash from './supervisour/SuppervisorDash'
 
-
-const SupApp =()=>{
-    return(
-        <>
-        <div>
-            <Routes>
-           <Route exact path={'/supDash'} element={<SuppervisorDash/>}/>
-            </Routes>    
-        </div>
-        </>
-    )
-
+const SupApp = () => {
+  return (
+    <>
+      <div>
+        <Routes>
+          <Route exact path={'/supDash'} element={<SuppervisorDash />} />
+          <Route exact path={'/acceptTopic'} element={<AcceptTopic />} />
+        </Routes>
+      </div>
+    </>
+  )
 }
-export default SupApp;
+export default SupApp
--- server/server.js
+++ server/server.js
@@ -1,32 +1,28 @@
-const express = require("express");
-const cors = require("cors");
+const express = require('express')
+const cors = require('cors')
 const mongoose = require('mongoose')
 
-require("dotenv").config({ path: "./config.env" });
+require('dotenv').config({ path: './config.env' })
 
-const app = express();
-const port = process.env.PORT || 5000;
+const app = express()
+const port = process.env.PORT || 5000
 
-app.use(cors());
-app.use(express.json());
+app.use(cors())
+app.use(express.json())
 
-const adminfile = require("./routes/adminfile.routes")
-const markingScheme=require("./routes/markingScheme.routes")
-const group=require("./routes/group.routes")
-const user=require("./routes/user.routes")
-const topic=require("./routes/topic.routes")
-const mark=require("./routes/mark.routes")
-
-
+const adminfile = require('./routes/adminfile.routes')
+const markingScheme = require('./routes/markingScheme.routes')
+const group = require('./routes/group.routes')
+const user = require('./routes/user.routes')
+const topic = require('./routes/topic.routes')
+const mark = require('./routes/mark.routes')
 
 app.use('/adminfile', adminfile)
-app.use('/markingScheme',markingScheme)
-app.use('/group',group)
-app.use('/user',user)
-app.use('/topic',topic)
-app.use('/marks',mark)
-
-
+app.use('/markingScheme', markingScheme)
+app.use('/group', group)
+app.use('/user', user)
+app.use('/topic', topic)
+app.use('/marks', mark)
 
 const uri = process.env.ATLAS_URI
 mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
@@ -38,8 +34,8 @@ const connection = mongoose.connection
 connection.once('open', () => {
   console.log('MongoDB database connection established successfully')
 })
- 
+
 app.listen(port, () => {
   // perform a database connection when server starts
-  console.log(`Server is running on port: ${port}`);
-});
+  console.log(`Server is running on port: ${port}`)
+})
